ac8461f65107d5f1eab52b18d5d8ac897e4c6a80
Lars Tingelstad <lars.tingelstad@ntnu.no>
2017-08-25 16:51:57 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='rudolph',
+    version='0.0.0',
+    packages=find_packages(),
+    author='Lars Tingelstad',
+    author_email='lars.tingelstad@ntnu.no',
+    license='Apache License, Version 2.0',
+)
