fixed bug in state and covariance update

rudolph/dqmekf.py
@@ -18,16 +18,16 @@ from .quaternion import Quaternion
 from .dual_quaternion import DualQuaternion, recover
 
 Q = np.diag([0, 0, 0, 0, 0, 0, 0.0011, 0.0011,
-             0.0011, 0.00002, 0.00002, 0.00002])
+             0.0011, 0.00002, 0.00002, 0.00002]).astype(np.float32)
 
 R = np.diag([0.0000003513, 0.00000259, 0.000003196,
-             0.00000547, 0.00000498, 0.0001018])
+             0.00000547, 0.00000498, 0.0001018]).astype(np.float32)
 
 G = np.eye(12)
 G[:6, :6] *= -0.5
 
 H = np.zeros((6, 12))
-H[:6, :6] = np.eye(6).astype(np.float)
+H[:6, :6] = np.eye(6).astype(np.float32)
 
 
 def get_F(w):
@@ -48,7 +48,7 @@ def time_propagation(P, X, t):
     dq = X.x.conj * X.dx * -0.5
     q = (X.x + dq * t).normalized()
 
-    F = get_F(X.dx.vskew)
+    F = get_F(-X.dx.vskew)
 
     dP = F @ P + P @ F.T + G @ Q @ G.T
 
@@ -59,6 +59,7 @@ def time_propagation(P, X, t):
 
 def measurement_update(meas, P, X):
     K = P @ H.T @ np.linalg.inv(H @ P @ H.T + R)
+
     err = X.x.conj * meas
     err_red = err.v
     delta_err = K @ err_red

rudolph/dual_quaternion.py
@@ -99,7 +99,7 @@ class DualQuaternion(object):
         return DualQuaternion(self.real.conj, self.dual.conj)
 
     def __mul__(self, other):
-        if isinstance(other, (float, np.float64)):
+        if isinstance(other, (float, np.float32)):
             other = DualQuaternion(Quaternion(other, 0, 0, 0), Quaternion())
         return DualQuaternion(self.real * other.real,
                               self.real * other.dual + self.dual * other.real)

rudolph/quaternion.py
@@ -24,7 +24,7 @@ def skew(v):
 
 class Quaternion(object):
     def __init__(self, q0=0, q1=0, q2=0, q3=0):
-        self._data = np.array([q0, q1, q2, q3]).flatten().astype(np.float32)
+        self._data = np.array([q0, q1, q2, q3]).flatten()
 
     @property
     def left_matrix(self):

test/test.py
@@ -9,10 +9,6 @@ from rudolph import DualQuaternion as DQuat
 
 from rudolph.dqmekf import (G, time_propagation, State, measurement_update)
 
-timestamps = np.loadtxt('../datasets/timestamps.txt')
-estimates = np.loadtxt(
-    '../datasets/icp_estimates_withinit_NN10_icp10.txt').reshape(-1, 8)
-
 X = State()
 X.x = DQuat(Quat(1, 0, 0, 0), Quat(0, 0, 0, 0))
 X.dx = DQuat()
@@ -24,6 +20,10 @@ q_icp = np.loadtxt(
     '../datasets/icp_estimates_withinit_NN10_icp10.txt').reshape(-1, 8)
 
 a = []
+b = []
+c = []
+
+d = []
 
 filts2 = np.loadtxt('../../DQ_filter_lib/filts.txt')
 xyzs = []
@@ -37,33 +37,49 @@ filts = []
 
 t_s = t[0]
 for x in range(1, 197):
-    time_propagation(P, X, t[x] - t_s)
+    X, P = time_propagation(P, X, t[x] - t_s)
     q = q_icp[x - 1]
     meas = DQuat(Quat(*q[:4]), Quat(*q[4:]))
-    measurement_update(meas, P, X)
+    X, P = measurement_update(meas, P, X)
 
+
+
+    print(X.x)
+    print(meas)
+    print(DQuat(Quat(*filts2[x-1][:4]), Quat(*filts2[x-1][4:8])))
+    print()
 
     filts.append(X.x.trs().v)
     measts.append(meas.trs().v)
 
+    d.append((DQuat(Quat(*filts2[x-1][:4]), Quat(*filts2[x-1][4:8])).trs().v.flatten()))
+
+
     a.append(X.x.asarray().flatten())
+    b.append(q.flatten())
+    c.append(filts2[x-1][:8])
 
     t_s = t[x]
 
 a = np.array(a).reshape(-1, 8)
+b = np.array(b).reshape(-1, 8)
+c = np.array(c).reshape(-1, 8)
+d = np.array(d).reshape(-1,3)
 
 f, axarr = plt.subplots(4, sharex=True)
 for i in range(4):
-    axarr[i].plot(q_icp[:, i])
+    # axarr[i].plot(q_icp[:, i])
     axarr[i].plot(a[:, i])
+    axarr[i].plot(b[:, i])
+    axarr[i].plot(c[:, i])
 
-measts = np.array(measts).reshape(-1,3) * 1000
-filts = np.array(filts).reshape(-1,3) * 1000
+measts = np.array(measts).reshape(-1,3)
+filts = np.array(filts).reshape(-1,3)
 
 f1, axarr1 = plt.subplots(3, sharex=True)
 for i in range(3):
     axarr1[i].plot(measts[:, i])
     axarr1[i].plot(filts[:, i])
-    # axarr1[i].plot(xyzs[:,i])
+    axarr1[i].plot(d[:,i])
 
 plt.show()